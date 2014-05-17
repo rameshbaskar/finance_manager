4cd1b65c37427274fbdae985fcdddb8334392243
Ramesh Baskarasubramanian <ramesh.baskarasubramanian@live.com>
2014-05-17 12:03:55 +0800

    Initial project setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FinanceManager::Application.config.secret_key_base = 'b2f429c217e8f8b000593e9c2a448dae3c417283eb5fe85586f64183a0dd5b836fadae49dec4c78fc680eb66b9272605d0ec42a2a44ac1056a9f548c89d66e37'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FinanceManager::Application.config.session_store :cookie_store, key: '_finance_manager_session'
