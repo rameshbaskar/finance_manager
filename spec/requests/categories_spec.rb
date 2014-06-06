9a011ad29e8f29bf358c60b53d35d0aec71abb6e
Ramesh Baskarasubramanian <ramesh.baskarasubramanian@live.com>
2014-06-07 04:29:17 +0800

    Refactored categories_request_spec

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -2,25 +2,24 @@ require 'spec_helper'
 
 describe 'Categories' do
   describe 'GET /categories' do
-    it 'works! (now write some real specs)' do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    let(:category) {Category.create!(name: 'test')}
+
+    it 'should work for index' do
       get categories_path
       response.status.should be(200)
     end
 
-    it 'works for /new' do
+    it 'should work for /new' do
       get "#{categories_path}/new"
       response.status.should be(200)
     end
 
-    it 'works for /show' do
-      category = Category.create!(:name => 'test')
+    it 'should work for /show' do
       get "#{categories_path}/#{category.id}"
       response.status.should be(200)
     end
 
-    it 'works for /edit' do
-      category = Category.create!(:name => 'test')
+    it 'should work for /edit' do
       get "#{categories_path}/#{category.id}/edit"
       response.status.should be(200)
     end
