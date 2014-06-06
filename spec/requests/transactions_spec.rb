4f881b609d10026639efc82164cf589d4ed8c376
Ramesh Baskarasubramanian <ramesh.baskarasubramanian@live.com>
2014-06-07 04:29:58 +0800

    Refactored transactions request spec and added more tests

--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -1,10 +1,26 @@
 require 'spec_helper'
 
-describe "Transactions" do
-  describe "GET /transactions" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+describe 'Transactions' do
+  describe 'GET /transactions' do
+    let(:transaction) {Transaction.create!(name: 'test', category: Category.find_by_id(1), day: '23', month: Month.find_by_name('June'), year: '2014', amount: 24001.99)}
+
+    it 'should work for index' do
       get transactions_path
+      response.status.should be(200)
+    end
+
+    it 'should work for /new' do
+      get "#{transactions_path}/new"
+      response.status.should be(200)
+    end
+
+    it 'should work for /show' do
+      get "#{transactions_path}/#{transaction.id}"
+      response.status.should be(200)
+    end
+
+    it 'should work for /edit' do
+      get "#{transactions_path}/#{transaction.id}/edit"
       response.status.should be(200)
     end
   end
